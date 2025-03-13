fa6978633d1ba82ed9cd99bac506e05f8271d0af
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2025-03-14 01:49:59 +0300

    support obsidian url for images

--- src/domain/image_utils.ts
+++ src/domain/image_utils.ts
@@ -1,6 +1,11 @@
 import { TFile, type App } from "obsidian";
 
 export async function getImageSource(app: App, imageName: string): Promise<string> {
+    if (isObsidianUrl(imageName)) {
+        const params = new URLSearchParams(imageName.split('?')[1]);
+        const filePath = decodeURIComponent(params.get("file") || "");
+        return app.vault.adapter.getResourcePath(filePath);
+    } else 
     if (isLocalPath(imageName)) {
         const localPath = await getImagePath(app, imageName);
         if (localPath) {
@@ -14,7 +19,11 @@ export async function getImageSource(app: App, imageName: string): Promise<strin
 }
 
 function isLocalPath(path: string) {
-    return !path.startsWith('http://') && !path.startsWith('https://');
+    return !path.startsWith('http://') && !path.startsWith('https://')
+}
+
+function isObsidianUrl(path: string) {
+    return path.startsWith('obsidian://');
 }
 
 async function getImagePath(app: App, imageName: string): Promise<string | null> {
